93d199225e1704c179e9ab632e469dbb6381469c
Matias Pinto <matias25pinto@gmail.com>
2024-11-16 10:23:25 -0300

    Agregar los funcional components de loadingMessages y PokemonCard

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,15 +1,30 @@
 import React from 'react'
-import { useFetch } from '../hooks'
+import { useCounter, useFetch } from '../hooks'
+import { LoadingMessage } from './LoadingMessage';
+import { PokemonCard } from './PokemonCard';
 
 export const MultipleCustomHooks = () => {
-    const { data, hasError, isLoading } = useFetch('https://pokeapi.co/api/v2/pokemon/1');
+    const {counter, increment, decremet} = useCounter(1);
+    const { data, hasError, isLoading } = useFetch(`https://pokeapi.co/api/v2/pokemon/${ counter }`);
   return (
     <>
         <h1>Información Pokemon</h1>
         <hr />
-        { isLoading && <p>Cargando...</p> }
+        { isLoading ? <LoadingMessage /> : <PokemonCard 
+          id={counter} 
+          name={data?.name} 
+          sprites={[
+            data.sprites.front_default,
+            data.sprites.front_shiny,
+            data.sprites.back_default,
+            data.sprites.back_shiny,
+          ]}
+          /> }
         {/* { data &&  <pre>{JSON.stringify(data, null, 2)}</pre> } */}
-        <h2>{data?.name}</h2>
+
+        <button className='btn btn-primary mt-3' onClick={() => (counter > 1) ? decremet() : null}>Anterior</button>
+
+        <button className='btn btn-primary mt-3' onClick={() => increment() }>Siguiente</button>
        
     </>
    )
